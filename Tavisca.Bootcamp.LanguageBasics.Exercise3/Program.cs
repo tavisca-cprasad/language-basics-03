405ebbe09658575e9d363f14c1f2d9194effc2c9
Chandan-97 <cp564738@gmail.com>
2019-06-24 14:30:08 +0530

    Excercise3 Completed

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -40,8 +40,79 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
-            // Add your code here.
+        	// Add your code here.
+        	int n = protein.Length;
+            int[] ans = new int[dietPlans.Length];
+            for(int i=0; i<dietPlans.Length; i++){
+                var diet = dietPlans[i];
+                ans[i] = solve(protein, carbs, fat, diet);
+            }
+            return ans;
             throw new NotImplementedException();
+        }
+
+        public static int solve(int[] protein, int[] carbs, int[] fat, string diet){
+            int[] calorie = new int[fat.Length];
+            for(int i=0; i<fat.Length; i++){
+                calorie[i] = fat[i] * 9 + (carbs[i] + protein[i]) * 5;
+            }
+            List<int> food = new List<int>();
+            for(int i=0; i<protein.Length; i++){
+                food.Add(i);
+            }
+
+            foreach(char ch in diet){
+                if(ch >= 'A' && ch <= 'Z'){
+                    if(ch == 'P') food = foodMax(food, protein);
+                    if(ch == 'C') food = foodMax(food, carbs);
+                    if(ch == 'F') food = foodMax(food, fat);
+                    if(ch == 'T') food = foodMax(food, calorie);
+                }
+                else{
+                    if(ch == 'p') food = foodMin(food, protein);
+                    if(ch == 'c') food = foodMin(food, carbs);
+                    if(ch == 'f') food = foodMin(food, fat);
+                    if(ch == 't') food = foodMin(food, calorie);
+                }
+
+                if(food.Count == 1) return food[0];
+            }
+
+            return food[0];
+        }
+
+        public static List<int> foodMax(List<int> idx, int[] a){
+            var maxval = a[idx[0]];
+            for(int i=1; i<idx.Count; i++)
+                maxval = Math.Max(maxval, a[idx[i]]);
+            
+            List<int> toremove = new List<int>();
+            foreach(var i in idx){
+                if(a[i] != maxval) 
+                    toremove.Add(i);
+            }
+
+            foreach(var i in toremove)
+                idx.Remove(i);
+
+            return idx;
+        }
+
+        public static List<int> foodMin(List<int> idx, int[] a){
+            var maxval = a[idx[0]];
+            for(int i=1; i<idx.Count; i++)
+                maxval = Math.Min(maxval, a[idx[i]]);
+            
+            List<int> toremove = new List<int>();
+            foreach(var i in idx){
+                if(a[i] != maxval) 
+                    toremove.Add(i);
+            }
+
+            foreach(var i in toremove)
+                idx.Remove(i);
+
+            return idx;
         }
     }
 }
